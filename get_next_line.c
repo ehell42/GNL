bfab21b78f211dfcf5c6b750e1f5ec95add62869
ehell42 <55402050+ehell42@users.noreply.github.com>
2019-11-09 19:56:27 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ehell <ehell@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 13:57:34 by ehell             #+#    #+#             */
-/*   Updated: 2019/10/28 15:22:13 by ehell            ###   ########.fr       */
+/*   Updated: 2019/11/09 19:54:25 by ehell            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,31 +88,55 @@ char	*ft_strskip(char *s)
 	return (tmp2);
 }
 
+t_file	*find_elem(t_file *file_mass, int fd)
+{
+	t_file	*tmp1;
+	t_file	tmp2;
+
+	tmp1 = file_mass;
+	while (tmp1)
+		if (tmp1->fd == fd)
+			return (tmp1);
+		else
+			tmp1 = tmp1->next;
+	tmp2.fd = fd;
+	tmp2.cont = ft_strnew(1);
+	tmp2.next = file_mass;
+	file_mass = &tmp2;
+	return (file_mass);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	static	char	*file_mass[999];
+	static	t_file	*file_mass;
+	t_file			*file;
 	char			*buff;
+	char			*tmp;
 	int				r;
 
 	if (fd < 0 || BUFF_SIZE <= 0 || !line ||
 		!(buff = ft_memalloc(BUFF_SIZE + 1)))
 		return (-1);
-	if (!file_mass[fd])
-		file_mass[fd] = ft_strnew(1);
+	file = find_elem(file_mass, fd);
 	r = 1;
-	while (!ft_strchr(buff, '\n') && !ft_strchr(file_mass[fd], '\n') && r != 0)
+	while (!ft_strchr(buff, '\n') && !ft_strchr(file->cont, '\n')
+		&& r != 0)
 	{
 		ft_bzero(buff, BUFF_SIZE + 1);
+		ft_putnbr(ft_strlen(file->cont));
+		ft_putchar('\n');
 		if ((r = read(fd, buff, BUFF_SIZE)) == -1)
 			return (-1);
-		if (!(file_mass[fd] = ft_myjoin(file_mass[fd], buff)))
+		ft_putnbr(ft_strlen(file->cont));					////////!!!!!!!JFEO???????????????????????DTYJTYJTY
+		if (!(file->cont = ft_myjoin(file->cont, buff)))
 			return (-1);
-		if (r == 0 && file_mass[fd][0] == '\0')
+		ft_putstr("here");
+		if (r == 0 && !file->cont)
 			return (0);
 	}
 	free(buff);
 	buff = NULL;
-	*line = ft_mycpy(file_mass[fd]);
-	file_mass[fd] = ft_strskip(file_mass[fd]);
+	*line = ft_mycpy(file->cont);
+	file->cont = ft_strskip(file->cont);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ehell <ehell@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:29:02 by ehell             #+#    #+#             */
-/*   Updated: 2019/10/21 15:20:45 by ehell            ###   ########.fr       */
+/*   Updated: 2019/11/09 18:49:19 by ehell            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,11 @@
 # define BUFF_SIZE 100
 
 int					get_next_line(const int fd, char **line);
+typedef struct		s_file
+{
+	char			*cont;
+	struct s_file	*next;
+	int				fd;
+}					t_file;
 
 #endif
