c6c96344456d5c305d3817a1a1a7f241790df198
ehell42 <55402050+ehell42@users.noreply.github.com>
2019-11-18 20:43:34 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: ehell <ehell@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 13:57:34 by ehell             #+#    #+#             */
-/*   Updated: 2019/11/11 17:30:43 by ehell            ###   ########.fr       */
+/*   Updated: 2019/11/18 20:42:48 by ehell            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		ft_myjoin(char *s1, int fd)
+int		ft_myjoin(char **s1, int fd)
 {
 	char	*tmp;
 	int		i;
@@ -23,11 +23,10 @@ int		ft_myjoin(char *s1, int fd)
 		return (-1);
 	if ((i = read(fd, buff, BUFF_SIZE)) == -1)
 		return (-1);
-			ft_putstr("here");
-	tmp = ft_strjoin(tmp, buff);
-	free(s1);
-	s1 = tmp;
-	ft_putstr(tmp);
+	tmp = ft_strjoin(*s1, buff);
+	free(*s1);
+	*s1 = tmp;
+	free(buff);
 	return (i);
 }
 
@@ -78,22 +77,24 @@ char	*ft_strskip(char *s)
 	return (tmp2);
 }
 
-t_file	*find_elem(t_file *file_mass, int fd)
+t_file	*find_elem(t_file **file_mass, int fd)
 {
 	t_file	*tmp1;
 	t_file	tmp2;
 
-	tmp1 = file_mass;
+	tmp1 = *file_mass;
+//	ft_putnbr((int)(*file_mass)->fd);
+//	ft_putchar('\n');
 	while (tmp1)
-		if (tmp1->fd == fd)
+		if ((*tmp1).fd == fd)
 			return (tmp1);
 		else
 			tmp1 = tmp1->next;
 	tmp2.fd = fd;
 	tmp2.cont = ft_strnew(1);
-	tmp2.next = file_mass;
-	file_mass = &tmp2;
-	return (file_mass);
+	tmp2.next = *file_mass;
+	*file_mass = &tmp2;
+	return (*file_mass);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -105,15 +106,14 @@ int		get_next_line(const int fd, char **line)
 
 	if (fd < 0 || BUFF_SIZE <= 0 || !line)
 		return (-1);
-	file = find_elem(file_mass, fd);
+	file = find_elem(&file_mass, fd);
 	tmp = file->cont;
 	r = 1;
 	while (!ft_strchr(tmp, '\n') && r != 0)
 	{
-		ft_putstr("here");
-		if ((r = ft_myjoin(tmp, file->fd)) < 0)
+		if ((r = ft_myjoin(&tmp, fd)) < 0)
 			return (-1);
-		if (r == 0 || !tmp)
+		if (r == 0 && tmp[0] == '\0')
 			return (0);
 	}
 	*line = ft_mycpy(tmp);
